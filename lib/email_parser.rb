f62136d2250b1300b8a4a7d64110c21ba5a9c7d4
Marcus Lau <lau.marcus76@gmail.com>
2020-05-05 18:00:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
   
   # parses commas, spaces, and rejects duplicate emails
   def parse
-    email_arr = @emails.split(/, /).uniq # splits with , and " " as well as .uniq to remove duplicate
+    email_arr = @emails.split(/[,\s]+/).uniq # splits with , and " " as well as .uniq to remove duplicate
     email_arr.reject! {|element| element.empty?}
     email_arr
   end
